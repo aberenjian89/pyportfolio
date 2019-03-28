91b231f90f36c52433823d51185ca39a319f0bef
Ali Berenjian <ali.rberenjian@gmail.com>
2019-03-28 11:56:18 -0700

    Complete Jobs Implementation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateField(default=date.today(), blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
